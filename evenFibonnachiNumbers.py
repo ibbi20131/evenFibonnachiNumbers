9b147a3ac52374b5671a0235eb0c651cd048b621
ibbi20131 <44727739+ibbi20131@users.noreply.github.com>
2019-01-19 10:42:16 +0400

    Add files via upload

---
+++ evenFibonnachiNumbers.py
@@ -0,0 +1,11 @@
+def evenFibonnachiNumbers(max):
+    fibonnachi = [1,2]
+    for i in range(max):
+        if i>1:
+            fibonnachi.append((fibonnachi[i-1])+(fibonnachi[i-2]))
+    count = 0
+    for i in fibonnachi:
+        if i%2 == 0:
+            count = count+i
+    return(count)
+print(evenFibonnachiNumbers(33))
